67389565e7201930a08b4309d432059d6f0918db
thanhdat1047 <thanhdat1047t@gmail.com>
2023-06-10 23:45:03 +0700

    up query sql

---
+++ quiz.sql
@@ -0,0 +1,101 @@
+INSERT INTO Questions(Content)
+VALUES ('Question1');
+INSERT INTO Questions(Content)
+VALUES ('Question2');
+INSERT INTO Questions(Content)
+VALUES ('Question3');
+INSERT INTO Questions(Content)
+VALUES ('Question4');
+
+INSERT INTO Questions(Content)
+VALUES ('Question5');
+INSERT INTO Questions(Content)
+VALUES ('Question6');
+INSERT INTO Questions(Content)
+VALUES ('Question7');
+INSERT INTO Questions(Content)
+VALUES ('Question8');
+INSERT INTO Questions(Content)
+VALUES ('Question9');
+INSERT INTO Questions(Content)
+VALUES ('Question10');
+
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (2,'an2.2','true');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (2,'an2.2','true');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (2,'an2.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (2,'an2.4','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (3,'an3.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (3,'an3.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (3,'an3.3','true');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (3,'an3.4','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (4,'an4.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (4,'an4.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (4,'an4.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (4,'an4.4','true');
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (5,'an5.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (5,'an5.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (5,'an5.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (5,'an5.4','true');
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (6,'an6.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (6,'an6.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (6,'an6.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (6,'an6.4','true');
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (7,'an7.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (7,'an7.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (7,'an7.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (7,'an7.4','true');
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (8,'an8.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (8,'an8.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (8,'an8.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (8,'an8.4','true');
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (9,'an9.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (9,'an9.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (9,'an9.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (9,'an9.4','true');
+
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (10,'an10.1','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (10,'an10.2','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (10,'an10.3','false');
+INSERT INTO Answers(QuestionId,Content,isCorrect)
+VALUES (10,'an10.4','true');
